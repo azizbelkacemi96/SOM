da51126811e0171c300a8ebd77a18e1289c8d25a
azizbelkacemi96 <azizbelkacemi96@gmail.com>
2021-01-02 16:37:03 +0100

    ajout de fonctions

---  Functions.c
+++  Functions.c
@@ -57,3 +57,45 @@ void AficheData(Data* data,int Ligne,int collone){
 
     }
 }
+void max_v(double k)
+{
+    max=malloc(N_conf.n_in*sizeof(double));
+    int i;
+    for(i=0;i<N_conf.n_in;i++)
+        max[i]=moyenne_v[i]+k;
+}
+
+void min_v(double k)
+{
+    min=malloc(N_conf.n_in*sizeof(double));
+    int i;
+    for(i=0;i<N_conf.n_in;i++)
+        min[i]=moyenne_v[i]-k;
+}
+/*
+Normalisation 
+*/
+
+void Normalisation(int i,int size )
+{
+    double somme=0.;
+    int j;
+    for(j=0;j<size;j++)
+        somme+=SQR(vecteur_tab[i].argument[j]);
+    vecteur_tab[i].norme=sqrt(somme);
+}
+
+void vecteur_moyen (int n)
+{
+    moyenne_v=malloc(N_conf.n_in*sizeof(double));
+    memset(moyenne_v,0,N_conf.n_in*sizeof(double));
+
+    int i,j;
+
+    for(i=0;i<N_conf.n_in;i++)
+    {
+        for(j=0;j<n;j++)
+            moyenne_v[i]+=vecteur_tab[j].argument[i];
+        moyenne_v[i]/=n;
+    }
+}
